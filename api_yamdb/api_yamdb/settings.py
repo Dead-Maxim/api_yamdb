2ab62badd4f844813b7ab426c496fb0912c169fb
Sergei Baranov <52358117+sergei-baranov@users.noreply.github.com>
2023-08-30 22:00:16 +0300

    Feature/auth n reg.phase 2 (#2)
    * расширение модели User - надо вмержить прежде всего, до первых миграций
    
    * set DEFAULT_AUTO_FIELD global value
    
    * rename custom users app
    
    * кажется без этого миграции приложения не подхватываются общим makemigrations, несмотря на наличие приложения в installed_apps
    
    * junior to admin fixed

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'extusers.apps.ExtusersConfig',
 ]
 
 MIDDLEWARE = [
@@ -101,3 +102,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'extusers.CustomUser'
---
+++ api_yamdb/extusers/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUser(AbstractUser):
+    class Roles(models.TextChoices):
+        USER = 'user', _('User')
+        MODERATOR = 'moderator', _('Moderator')
+        ADMIN = 'admin', _('Admin')
+
+    bio = models.TextField('Биография', blank=True)
+    role = models.CharField(
+        max_length=16,
+        choices=Roles.choices,
+        default=Roles.USER,
+    )
