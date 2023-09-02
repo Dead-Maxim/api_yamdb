467290b137ea1795f596b7814411305579ea8108
Dead-Maxim <120676116+Dead-Maxim@users.noreply.github.com>
2023-09-02 17:22:01 +0300

    title (#8)

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,9 +1,53 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
+from datetime import datetime
 
 User = get_user_model()
 
 TEXT_LEN = 50
+MIN_YEAR = 1
+YEAR_NOW = datetime.now().year
+TITLE_LEN = 100
+
+
+class Title(models.Model):
+    name = models.CharField(
+        "Наименование произведения", max_length=TITLE_LEN
+    )
+    year = models.PositiveSmallIntegerField(
+        "Год выпуска произведения",
+        db_index=True,
+        validators=[
+            MinValueValidator(MIN_YEAR),
+            MaxValueValidator(YEAR_NOW),
+        ],
+    )
+    description = models.TextField(
+        "Описание произведения", null=True, blank=True
+    )
+    genre = models.ManyToManyField(
+        "Genre",
+        verbose_name="Жанр произведения",
+        blank=True,
+        related_name="titles",
+    )
+    category = models.ForeignKey(
+        "Category",
+        verbose_name="Категория произведения",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="titles",
+    )
+
+    class Meta:
+        verbose_name = "Произведение"
+        verbose_name_plural = "Произведения"
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name[:TEXT_LEN]
 
 
 class Reviews(models.Model):
