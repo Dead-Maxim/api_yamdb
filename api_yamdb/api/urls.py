f72069ff30ba4cb88a36aad40bd88f9d5e7efdbc
Sergei Baranov <52358117+sergei-baranov@users.noreply.github.com>
2023-09-05 16:37:08 +0300

    Feature/reviews.unique together (#14)
    * работаем эндпойнт /users/me/
    
    * /users/me/ PATCH
    
    * /users/ endpoints, AdminsHard permission
    
    * /users/ pagination
    
    * /users/-n-/auth/ flake8 fixes
    
    * подтянули ендпойнты reviews по тестам

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,8 +3,7 @@ from rest_framework.routers import SimpleRouter
 
 from extusers.views import MeViewSet, SignupViewSet, TokenViewSet, UsersViewSet
 from api.views import (CategoryViewSet, GenreViewSet, TitleViewSet,
-                       ReviewsViewSet)
-from extusers.views import SignupViewSet, TokenViewSet
+                       ReviewViewSet)
 
 
 router = SimpleRouter()
@@ -13,7 +12,7 @@ router.register(r'v1/auth/token', TokenViewSet, basename='token')
 router.register(r'v1/genres', GenreViewSet, basename="genres")
 router.register(r'v1/categories', CategoryViewSet, basename="categories")
 router.register(r'v1/titles', TitleViewSet, basename="titles")
-router.register(r'v1/titles/(?P<title_id>[^/.]+)/reviews', ReviewsViewSet,
+router.register(r'v1/titles/(?P<title_id>[^/.]+)/reviews', ReviewViewSet,
                 basename="reviews")
 router.register(r'v1/users', UsersViewSet, basename="users")
 
